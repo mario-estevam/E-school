9786ec3d822fc0cb4605b0308089c36152639106
dalania <jdalaniasilva@gmail.com>
2022-05-01 14:01:06 -0300

    colocado required nas label para os campos obrigatorios.

--- src/main/resources/static/js/form-validation.js
+++ src/main/resources/static/js/form-validation.js
@@ -1,37 +1,27 @@
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
-    'use strict';
-
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation');
-
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms).forEach((form) => {
-        form.addEventListener('submit', (event) => {
-            if (!form.checkValidity()) {
-                event.preventDefault();
-                event.stopPropagation();
-            }
-            form.classList.add('was-validated');
-        }, false);
-    });
-})();
-
 const buttonSubmitUser = document.querySelector('.submit-form-user');
+
+
 
 buttonSubmitUser.addEventListener('click', validate)
 
 function validate(e){
     // e.preventDefault();
 
-    const inputsPageC = document.querySelectorAll('input[required]');
+    const inputsPageC = document.querySelectorAll('input[required], select[required]');
 
     inputsPageC.forEach((item)=>{
-        item.classList.add('is-invalid')
+        if(item.value === ""){
+            item.classList.add('is-invalid')
+
+        }
+
     })
 
 
-
-
 }
+
+$(document).ready(function(){
+    $('.cpf2').mask('000.000.000-00');
+    $('.rg').mask('000.000.000')
+});
