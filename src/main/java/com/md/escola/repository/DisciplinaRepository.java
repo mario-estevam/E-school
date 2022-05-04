a27a6828a04739b5a5a051af4fa81085a1292bfb
danielbrasc <carlosbrasnascimento@gmail.com>
2022-05-04 17:52:28 -0300

    Adicionei um metódo no service para listar todas as disciplinas

--- src/main/java/com/md/escola/repository/DisciplinaRepository.java
+++ src/main/java/com/md/escola/repository/DisciplinaRepository.java
@@ -2,6 +2,8 @@ package com.md.escola.repository;
 
 import com.md.escola.models.Disciplina;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DisciplinaRepository extends JpaRepository<Disciplina,Long> {
 }
--- src/main/java/com/md/escola/service/DisciplinaService.java
+++ src/main/java/com/md/escola/service/DisciplinaService.java
@@ -1,9 +1,13 @@
 package com.md.escola.service;
 
 import com.md.escola.models.Disciplina;
+import com.md.escola.models.Pessoa;
 import com.md.escola.repository.DisciplinaRepository;
+import com.md.escola.repository.ProfessorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DisciplinaService {
@@ -11,7 +15,15 @@ public class DisciplinaService {
     @Autowired
     DisciplinaRepository repository;
 
+    @Autowired
+    private void setDisciplinaRepository(DisciplinaRepository repository){ this.repository = repository; }
+
     public Disciplina insert(Disciplina disciplina){
         return repository.save(disciplina);
     }
+
+    public List<Disciplina> getAll(){
+        return repository.findAll();
+    }
+
 }
