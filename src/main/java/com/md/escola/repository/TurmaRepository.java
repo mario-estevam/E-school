aaa2d5bfc38d8d53ebdf8a597448be93479c2d7a
danielbrasc <carlosbrasnascimento@gmail.com>
2022-05-04 20:50:08 -0300

    Adicionei o repository e o service da turma

---
+++ src/main/java/com/md/escola/repository/TurmaRepository.java
@@ -0,0 +1,7 @@
+package com.md.escola.repository;
+
+import com.md.escola.models.Turma;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TurmaRepository extends JpaRepository<Turma,Long> {
+}
---
+++ src/main/java/com/md/escola/service/TurmaService.java
@@ -0,0 +1,29 @@
+package com.md.escola.service;
+
+import com.md.escola.models.Professor;
+import com.md.escola.models.Turma;
+import com.md.escola.repository.TurmaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TurmaService {
+
+    TurmaRepository repository;
+
+    @Autowired
+    DisciplinaService disciplinaService;
+
+    @Autowired
+    ProfessorService professorService;
+
+    @Autowired
+    private void setTurmaRepository(TurmaRepository repository){ this.repository = repository; }
+
+    public Turma insert(Turma turma, Long id){
+        Professor professor = professorService.getId(id);
+        turma.setProfessor(professor);
+        return repository.save(turma);
+    }
+
+}
